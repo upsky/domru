ec216865386c8ddd92f53bbcadf0976693feb501
ssergey <sib.sergey@gmail.com>
2014-03-20 01:55:18 +0700

    Title Gui - completed

---
+++ Assets/Scripts/NguiTweens/ButtonApplicationQuit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonApplicationQuit : MonoBehaviour
+{
+    [SerializeField]
+    private float _quitTime = 0.3f;
+
+    private void Start()
+    {
+    }
+
+    private void OnClick()
+    {
+        Invoke("Quit", _quitTime);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/NguiTweens/ButtonOpenUrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonOpenUrl : MonoBehaviour
+{
+    [SerializeField]
+    private string _url = "http://www.domru.ru";
+
+    private void Start()
+    {
+    }
+
+    private void OnClick()
+    {
+        Application.OpenURL(_url);
+    }
+}
