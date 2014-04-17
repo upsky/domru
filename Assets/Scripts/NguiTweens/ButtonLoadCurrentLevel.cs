e16464fdf76335e08d04cf1917d705e06fda996e
ssergey <sib.sergey@gmail.com>
2014-04-17 15:03:51 +0700

    fix load next level buttons

---
+++ Assets/Scripts/NguiTweens/ButtonLoadCurrentLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonLoadCurrentLevel : MonoBehaviour
+{
+    //[SerializeField]
+    //private Consts.SceneNames _sceneName;
+
+    private void Start()
+    {
+    }
+
+    private void OnClick()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+}
---
+++ Assets/Scripts/NguiTweens/ButtonLoadNextLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonLoadNextLevel : MonoBehaviour
+{
+    //[SerializeField]
+    //private Consts.SceneNames _sceneName;
+
+    private void Start()
+    {
+    }
+
+    private void OnClick()
+    {
+        if (Application.loadedLevelName==Consts.SceneNames.Level1.ToString())
+            Application.LoadLevel(Consts.SceneNames.Level1.ToString());
+        else if (Application.loadedLevelName.Contains("Room"))
+        {
+            int level = int.Parse(Application.loadedLevelName.Remove(0, 4));
+
+            if (level == 9) //то грузить себя снова
+            {
+                Application.LoadLevel(Consts.SceneNames.Room9.ToString());
+            }
+            else
+            {
+                level++;
+                Application.LoadLevel("Room" + level);
+            }
+        }
+    
+    }
+}
