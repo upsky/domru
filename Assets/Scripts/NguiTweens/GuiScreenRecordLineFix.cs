a95620414308b8cf372d040beabf6a39fea44414
ssergey <sib.sergey@gmail.com>
2014-04-25 18:57:17 +0700

    fix gui in RecordsScene -in progress for android 90%

---
+++ Assets/Scripts/NguiTweens/GuiScreenRecordLineFix.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiScreenRecordLineFix : MonoBehaviour
+{
+    private UIRect _uiItem;
+
+	void Start ()
+	{
+        _uiItem = this.GetSafeComponent<UIRect>();
+
+        float w = Screen.width ;
+        float h = Screen.height;
+
+	    float res = h/w;
+        //Debug.LogWarning(res);
+
+        _uiItem.leftAnchor.target = _uiItem.rightAnchor.target = transform.root.Find("Panel1");
+
+	    _uiItem.leftAnchor.Set(0f, 0f);
+	    _uiItem.rightAnchor.Set(1f, 0f);
+
+        _uiItem.ResetAnchors();
+     
+	    //_uiItem.leftAnchor.
+	}
+
+    private void Update()
+    {
+        ////_uiItem.leftAnchor.Set(0f, 1f);
+        ////_uiItem.rightAnchor.Set(1f, 1f);
+        //_uiItem.UpdateAnchors();
+        
+    }
+
+
+}
