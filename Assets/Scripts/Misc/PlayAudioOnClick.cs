c9a392aa12db3ea715a4370ea4a29433b58363d6
ssergey <sib.sergey@gmail.com>
2014-05-15 18:46:09 +0700

    Add projector sprites changing

--- Assets/Scripts/Misc/PlayAudioOnClick.cs
+++ Assets/Scripts/Misc/PlayAudioOnClick.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayOnClick : MonoBehaviour
+public class PlayAudioOnClick : MonoBehaviour
 {
     [SerializeField]
     private AudioClip[] _clips;
---
+++ Assets/Scripts/Misc/SpriteChangingOnClick.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteChangingOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private Texture[] _sprites;
+
+    [SerializeField]
+    private Renderer _targetRenderer;
+
+    [SerializeField]
+    private float _duration = 1f;
+
+    private Texture _defaultSprite;
+
+    private void Start()
+    {
+        if (_targetRenderer == null)
+            Debug.LogError("targetRenderer not found", this);
+
+        _defaultSprite = _targetRenderer.material.mainTexture;
+    }
+
+
+    private void OnClick()
+    {
+        StartCoroutine(ChangeAlphaCoroutine());
+    }
+
+    private IEnumerator ChangeAlphaCoroutine()
+    {
+        var startTime = Time.time;
+        int currentSpriteIndex = 0;
+        while (_duration>(Time.time-startTime))
+        {
+            _targetRenderer.material.mainTexture = _sprites[currentSpriteIndex];
+            
+            currentSpriteIndex++;
+            if (currentSpriteIndex >= _sprites.Length)
+                currentSpriteIndex = 0;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _targetRenderer.material.mainTexture = _defaultSprite;
+    }
+
+}
