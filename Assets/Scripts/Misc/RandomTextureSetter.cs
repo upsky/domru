c3e2aedae826dcf72867cfe462e79e5d23de0b6d
ssergey <sib.sergey@gmail.com>
2014-05-15 20:13:13 +0700

    Add RandomTextureSetter

---
+++ Assets/Scripts/Misc/RandomTextureSetter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTextureSetter : MonoBehaviour
+{
+    [SerializeField]
+    private Texture[] _textures;
+
+    private void Start()
+    {
+        renderer.material.mainTexture = RandomUtils.GetRandomItem(_textures);
+    }
+
+}
