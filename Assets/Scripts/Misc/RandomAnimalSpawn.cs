5332afb5b253548910042975cc54770b74102207
ssergey <sib.sergey@gmail.com>
2014-05-19 16:55:42 +0700

    Setting animals

---
+++ Assets/Scripts/Misc/RandomAnimalSpawn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomAnimalSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _prefabs;
+
+	void Awake ()
+	{
+	    var prefab=RandomUtils.GetRandomItem(_prefabs);
+        var animal = (GameObject)Object.Instantiate(prefab, transform.position, new Quaternion(0, 0, 0, 0));
+	    animal.transform.parent = transform.parent;
+	}
+
+}
